9baad003923ff6a7b08dafb964b4bc37e8600b4e
papabn <pndiaye999@gmail.com>
2025-03-21 01:48:49 +0000

    add new SVG assets and update authentication pages with Clerk integration

--- lets-talk/app/(auth)/login/[[...login]]/page.tsx
+++ lets-talk/app/(auth)/login/[[...login]]/page.tsx
@@ -1,9 +1,33 @@
+import { SignIn } from '@clerk/nextjs'
+import { neobrutalism } from '@clerk/themes'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <main className='flex flex-col items-center p-5 gap-10 animate-fade-in'>
+        <section className="flex flex-col items-center">
+            <Image
+                src="/assets/logo.svg"
+                alt="Logo"
+                width={100}
+                height={100}
+
+            />
+            <h1 className="text-lg font-extrabold  lg:text-2xl">
+                connect , communicate and share your ideas with the world
+            </h1>
+        </section>
+        <div className="mt-3">
+            <SignIn 
+                appearance={
+                    {
+                        baseTheme: neobrutalism
+                    }
+                }
+            />
+        </div>
       
-    </div>
+    </main>
   )
 }
--- lets-talk/app/(auth)/register/[[...register]]/page.tsx
+++ lets-talk/app/(auth)/register/[[...register]]/page.tsx
@@ -1,9 +1,33 @@
+import { SignUp } from '@clerk/nextjs'
+import { neobrutalism } from '@clerk/themes'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      
-    </div>
+     <main className='flex flex-col items-center p-5 gap-10 animate-fade-in'>
+           <section className="flex flex-col items-center">
+               <Image
+                   src="/assets/logo.svg"
+                   alt="Logo"
+                   width={100}
+                   height={100}
+   
+               />
+               <h1 className="text-lg font-extrabold  lg:text-2xl">
+                   connect , communicate and share your ideas with the world
+               </h1>
+           </section>
+           <div className="mt-3">
+               <SignUp 
+                   appearance={
+                       {
+                           baseTheme: neobrutalism
+                       }
+                   }
+               />
+           </div>
+         
+       </main>
   )
 }
