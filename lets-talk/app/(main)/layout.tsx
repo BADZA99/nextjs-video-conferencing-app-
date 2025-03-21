4cd8bd6506318ae4d84b9467ab8e9af9f3f3894a
papabn <pndiaye999@gmail.com>
2025-03-21 02:11:34 +0000

    add main and home layouts with authentication handling and update home page

---
+++ lets-talk/app/(main)/layout.tsx
@@ -0,0 +1,42 @@
+import { SignIn } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { neobrutalism } from "@clerk/themes";
+import Image from "next/image";
+
+export default async function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+    const user = await currentUser();
+    if (!user) {
+      return (
+        <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+          <section className="flex flex-col items-center">
+            <Image src="/assets/logo.svg" alt="Logo" width={100} height={100} />
+            <h1 className="text-lg font-extrabold  lg:text-2xl">
+              connect , communicate and share your ideas with the world
+            </h1>
+          </section>
+          <div className="mt-3">
+            <SignIn
+              /* routing="hash" est utilisé ici pour simplifier la gestion des routes d'authentification et éviter des conflits avec le routage de Next.js.*/
+              routing="hash"
+              appearance={{
+                baseTheme: neobrutalism,
+              }}
+            />
+          </div>
+        </main>
+      );
+    }
+  return (
+    <main className="relative">
+      {/* <Navbar/> */}
+
+      <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <div className="w-full">{children}</div>
+      </section>
+    </main>
+  );
+}
